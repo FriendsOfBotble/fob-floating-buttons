f063ffe6228f84159a24ec1181d89255f07effd8
Sang Nguyen <sangnguyenplus@gmail.com>
2025-07-12 16:23:26 +0700

    Improve code

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,16 @@
 <?php
 
-use Botble\Base\Facades\BaseHelper;
+use Botble\Base\Facades\AdminHelper;
 use FriendsOfBotble\FloatingButtons\Http\Controllers\Settings\FloatingButtonsSettingController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix(BaseHelper::getAdminPrefix() . '/floating-buttons')
-    ->name('fob-floating-buttons.settings')
-    ->middleware(['core', 'web', 'auth'])
-    ->group(function () {
-        Route::group(['permission' => 'fob-floating-buttons.settings'], function () {
-            Route::get('/', [FloatingButtonsSettingController::class, 'edit']);
-            Route::put('/', [FloatingButtonsSettingController::class, 'update'])->name('.update');
-        });
+AdminHelper::registerRoutes(function () {
+    Route::group([
+        'permission' => 'fob-floating-buttons.settings',
+        'as' => 'fob-floating-buttons.settings.',
+        'prefix' => 'settings/floating-buttons',
+    ], function () {
+        Route::get('/', [FloatingButtonsSettingController::class, 'edit'])->name('edit');
+        Route::put('/', [FloatingButtonsSettingController::class, 'update'])->name('update');
     });
+});
--- src/Providers/FloatingButtonsServiceProvider.php
+++ src/Providers/FloatingButtonsServiceProvider.php
@@ -2,11 +2,12 @@
 
 namespace FriendsOfBotble\FloatingButtons\Providers;
 
-use Botble\Base\Facades\DashboardMenu;
+use Botble\Base\Facades\PanelSectionManager;
+use Botble\Base\PanelSections\PanelSectionItem;
 use Botble\Base\Supports\ServiceProvider;
 use Botble\Base\Traits\LoadAndPublishDataTrait;
+use Botble\Setting\PanelSections\SettingOthersPanelSection;
 use Botble\Theme\Facades\Theme;
-use Illuminate\Routing\Events\RouteMatched;
 
 class FloatingButtonsServiceProvider extends ServiceProvider
 {
@@ -24,32 +25,54 @@ class FloatingButtonsServiceProvider extends ServiceProvider
             ->loadRoutes();
 
         $this->app->booted(function () {
-            $this->app['events']->listen(RouteMatched::class, function () {
-                DashboardMenu::make()
-                    ->registerItem([
-                        'id' => 'cms-plugins-floating-buttons',
-                        'priority' => 9999,
-                        'parent_id' => null,
-                        'name' => 'plugins/fob-floating-buttons::fob-floating-buttons.name',
-                        'icon' => 'ti ti-social',
-                        'url' => fn () => route('fob-floating-buttons.settings'),
-                        'permissions' => ['fob-floating-buttons.settings'],
-                    ]);
-
+            PanelSectionManager::default()->beforeRendering(function (): void {
+                PanelSectionManager::registerItem(
+                    SettingOthersPanelSection::class,
+                    fn () => PanelSectionItem::make('fob-floating-buttons')
+                        ->setTitle(trans('plugins/fob-floating-buttons::fob-floating-buttons.name'))
+                        ->withDescription(trans('plugins/fob-floating-buttons::fob-floating-buttons.description'))
+                        ->withIcon('ti ti-social')
+                        ->withPriority(9999)
+                        ->withRoute('fob-floating-buttons.settings.edit')
+                        ->withPermission('fob-floating-buttons.settings')
+                );
             });
 
             if (setting('fob-floating-buttons.enabled')) {
                 Theme::asset()
                     ->usePath(false)
-                    ->add('fob-floating-buttons-default-css', asset('vendor/core/plugins/fob-floating-buttons/css/default.min.css'), version: '1.0.1')
-                    ->add('fob-floating-buttons-css', asset('vendor/core/plugins/fob-floating-buttons/css/fob-floating-buttons.css'), version: '1.0.1');
+                    ->add(
+                        'fob-floating-buttons-default-css',
+                        asset('vendor/core/plugins/fob-floating-buttons/css/default.min.css'),
+                        version: '1.0.1'
+                    )
+                    ->add(
+                        'fob-floating-buttons-css',
+                        asset('vendor/core/plugins/fob-floating-buttons/css/fob-floating-buttons.css'),
+                        version: '1.0.1'
+                    );
 
                 Theme::asset()
                     ->container('footer')
                     ->usePath(false)
-                    ->add('fob-velocity-js', asset('vendor/core/plugins/fob-floating-buttons/js/velocity.min.js'), ['jquery'], version: '1.0.1')
-                    ->add('fob-floating-buttons-default-js', asset('vendor/core/plugins/fob-floating-buttons/js/default.min.js'), ['jquery'], version: '1.0.1')
-                    ->add('fob-floating-buttons-js', asset('vendor/core/plugins/fob-floating-buttons/js/fob-floating-buttons.js'), ['jquery'], version: '1.0.1');
+                    ->add(
+                        'fob-velocity-js',
+                        asset('vendor/core/plugins/fob-floating-buttons/js/velocity.min.js'),
+                        ['jquery'],
+                        version: '1.0.1'
+                    )
+                    ->add(
+                        'fob-floating-buttons-default-js',
+                        asset('vendor/core/plugins/fob-floating-buttons/js/default.min.js'),
+                        ['jquery'],
+                        version: '1.0.1'
+                    )
+                    ->add(
+                        'fob-floating-buttons-js',
+                        asset('vendor/core/plugins/fob-floating-buttons/js/fob-floating-buttons.js'),
+                        ['jquery'],
+                        version: '1.0.1'
+                    );
 
                 add_filter(THEME_FRONT_FOOTER, function (?string $data): ?string {
                     $floatingButtons = setting('fob-floating-buttons.items');
@@ -62,7 +85,10 @@ class FloatingButtonsServiceProvider extends ServiceProvider
 
                     $collapsedOnMobile = setting('fob-floating-buttons.display_on_mobile', 'collapsed') == 'collapsed';
 
-                    return $data . view('plugins/fob-floating-buttons::floating-buttons', compact('floatingButtons', 'collapsedOnMobile'))->render();
+                    return $data . view(
+                        'plugins/fob-floating-buttons::floating-buttons',
+                        compact('floatingButtons', 'collapsedOnMobile')
+                    )->render();
                 }, 192);
             }
         });
